f2560ee42145bb3a1be00c883268087eecef695c
seiwynn <45825650+seiwynn@users.noreply.github.com>
2024-09-03 15:17:36 -0400

    commented git out so that I can run full script directly

--- ubuntu/setup_dev_stuff.sh
+++ ubuntu/setup_dev_stuff.sh
@@ -10,10 +10,11 @@ sudo apt install -y curl git build-essential
 git config --global core.editor "gedit --wait --new-window"
 
 # git credentials
-git config --global user.name "seiwynn"
-git config --global user.email "<your_id>+seiwynn@users.noreply.github.com"
-git config --global credential.helper store
+# git config --global user.name "seiwynn"
+# git config --global user.email "<your_id>+seiwynn@users.noreply.github.com"
+# git config --global credential.helper store
 # git credential approve < <(echo "protocol=https\nhost=github.com\nusername=seiwynn\npassword=your_token_here")
+
 # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
 # I used classic in my most recent setup, but either classic or beta should work.
 
